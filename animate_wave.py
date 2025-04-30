e558d21684c694c04e4fcf8b252872f1dd3f81b0
wasabi3D <tryexceptfinally5@gmail.com>
2025-04-30 20:12:16 +0200

    aaa

--- animate_wave.py
+++ animate_wave.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib
-matplotlib.use("MacOSX")
+#matplotlib.use("MacOSX")
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import os
 import subprocess
+from multiprocessing import Pool
 
 plt.rcParams.update({
     #"text.usetex": True,
@@ -30,7 +31,14 @@ CFL = 1.0
 h00= 4.0
 initialization="mode"
 
-cmd = f"{repertoire}{executable} {input_filename} f_hat={f_hat} h00={h00} CFL={CFL} initialization={initialization} L={L} tfin={tfin} nx={nx} n_init={n_init} output={output_base}"
+u = np.sqrt(h00*g)
+omega_n = (0.5+n_init) * np.pi * u/L
+Tn = 2*np.pi/omega_n
+
+tfin = Tn
+
+
+cmd = f"{repertoire}{executable} {input_filename} tfin={tfin} f_hat={f_hat} h00={h00} CFL={CFL} initialization={initialization} L={L} tfin={tfin} nx={nx} n_init={n_init} output={output_base}"
 subprocess.run(cmd, shell=True)
 
 
@@ -43,14 +51,13 @@ x = np.loadtxt(output_base + "_x")
 
 # === Préparer l'animation ===
 fig, ax = plt.subplots(figsize=(10, 5))
-line, = ax.plot(x, fx[0], lw=2)
+line, = ax.plot(x, fx[0], lw=2, label="Simul")
 # mode propre
 p_t = np.tile(times, (fx.shape[1], 1)).T
 p_x = np.tile(np.linspace(0, L, nx+1), (times.shape[0], 1))
 #print(p_t.shape, times.shape, fx.shape[1], p_x.shape)
-u = np.sqrt(h00*g)
-omega_n = (0.5+n_init) * np.pi * u/L
-p_fx = f_hat * np.cos(omega_n * p_t) * np.cos(p_x *omega_n / u)
+p_fx = f_hat * np.cos(omega_n * p_t) * np.cos(p_x * omega_n / u)
+p_line, = ax.plot(x, p_fx[0], lw=2, label="Analytique")
 
 time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
 
@@ -62,12 +69,13 @@ ax.set_ylim(np.min(fx)*1.1, np.max(fx)*1.1)
 
 def update(frame):
     line.set_ydata(fx[frame])
+    p_line.set_ydata(p_fx[frame])
     time_text.set_text(f"t = {times[frame]:.3f} s")
-    return line, time_text
+    return line, p_line, time_text
 
 ani = animation.FuncAnimation(fig, update, frames=len(times), interval=15, blit=True)
-#ani.save("anim.gif", fps=60)
 plt.tight_layout()
+
 
 plt.figure()
 mp = plt.imshow(fx, cmap='hot', extent=(0,L,tfin,0))
@@ -87,8 +95,9 @@ if CFL>1:
     plt.savefig(f"CFL={CFL}_x={x_idx*L/nx}.svg")
 
 # Question 5.3c)
-#if initialization=="mode":
-    
+if initialization=="mode":
+    pass
+
 
 plt.show()
 
---
+++ convergence.py
@@ -0,0 +1,86 @@
+import numpy as np
+import matplotlib
+#matplotlib.use("MacOSX")
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import os
+import subprocess
+from multiprocessing import Pool
+
+plt.rcParams.update({
+    #"text.usetex": True,
+    "font.family": "sans-serif",
+    "font.size": 16,
+})
+fs=16
+ls=14
+
+repertoire = "./"
+executable = "physnum5"
+input_filename = "configuration.in"
+# === Paramètres ===
+output_base = "test.out"
+
+
+
+g  = 9.81;
+f_hat = 1.0
+L=15
+tfin=10
+nx=20
+n_init=4
+CFL = 1.0
+h00= 4.0
+initialization="mode"
+
+
+u = np.sqrt(h00*g)
+omega_n = (0.5+n_init) * np.pi * u/L
+Tn = 2*np.pi/omega_n
+tfin = Tn
+nsteps=int(u*tfin/(L/nx)+100)
+
+paramstr="nx"
+nsimul=20
+param = np.arange(1, nsimul+1) * nx
+
+
+
+def _simulate(idx):
+    output = f"{output_base}_{idx}"
+    cmd = f"{repertoire}{executable} {input_filename} impose_nsteps=true tfin={tfin} f_hat={f_hat} h00={h00} CFL={CFL} initialization={initialization} L={L} tfin={tfin} nsteps={nsteps} nx={nx} n_init={n_init} {paramstr}={param[idx]} output={output}"
+    print(cmd)
+    subprocess.run(cmd, shell=True)
+    data_f = np.loadtxt(output + "_f")
+    times = data_f[:, 0]
+    fx = data_f[:, 1:]
+    x = np.loadtxt(output + "_x")
+
+    p_t = np.tile(times, (fx.shape[1], 1)).T
+    p_x = np.tile(np.linspace(0, L, fx.shape[1]), (times.shape[0], 1))
+    p_fx = f_hat * np.cos(omega_n * p_t) * np.cos(p_x * omega_n / u)
+    
+    dx = L/(fx.shape[1]-1)
+    error = np.sum(np.abs(fx[-1,:] - p_fx[-1,:]))*dx
+
+
+
+    print(f'Done. ({paramstr}={param[idx]})')
+    return error
+
+errors = np.zeros(nsimul)
+
+def plot():
+    plt.figure()
+    plt.plot(errors)
+    plt.show()
+
+if __name__ == '__main__':
+    batch_size = 10
+    for nbatch in range(0, nsimul, batch_size):
+        with Pool(processes=batch_size) as pool:
+            results = pool.map(_simulate, range(nbatch, min(nsimul, nbatch + batch_size)))
+            for i, res in enumerate(results):
+                errors[nbatch + 1] = results[0]
+    plot()
+
